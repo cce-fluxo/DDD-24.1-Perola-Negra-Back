32cb34effa9be0196b1a714f0ac98481bbd22dc5
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2024-09-09 20:14:56 -0300

    prismaservice bug fix

--- ddd-e-commerce/src/registro_pedido_produto/registro_pedido_produto.module.ts
+++ ddd-e-commerce/src/registro_pedido_produto/registro_pedido_produto.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegistroPedidoProdutoService } from './registro_pedido_produto.service';
 import { RegistroPedidoProdutoController } from './registro_pedido_produto.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RegistroPedidoProdutoController],
-  providers: [RegistroPedidoProdutoService],
+  providers: [RegistroPedidoProdutoService, PrismaService],
 })
 export class RegistroPedidoProdutoModule {}
