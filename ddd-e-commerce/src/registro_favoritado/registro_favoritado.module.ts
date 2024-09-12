214a0250018e60a737694c6f5daf1c6aad168ffa
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2024-09-12 10:50:51 -0300

    fix breno

--- ddd-e-commerce/src/registro_favoritado/registro_favoritado.module.ts
+++ ddd-e-commerce/src/registro_favoritado/registro_favoritado.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegistroFavoritadoService } from './registro_favoritado.service';
 import { RegistroFavoritadoController } from './registro_favoritado.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RegistroFavoritadoController],
-  providers: [RegistroFavoritadoService],
+  providers: [RegistroFavoritadoService, PrismaService],
 })
 export class RegistroFavoritadoModule {}
--- ddd-e-commerce/src/registro_produto_desconto/registro_produto_desconto.module.ts
+++ ddd-e-commerce/src/registro_produto_desconto/registro_produto_desconto.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegistroProdutoDescontoService } from './registro_produto_desconto.service';
 import { RegistroProdutoDescontoController } from './registro_produto_desconto.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RegistroProdutoDescontoController],
-  providers: [RegistroProdutoDescontoService],
+  providers: [RegistroProdutoDescontoService, PrismaService],
 })
 export class RegistroProdutoDescontoModule {}
