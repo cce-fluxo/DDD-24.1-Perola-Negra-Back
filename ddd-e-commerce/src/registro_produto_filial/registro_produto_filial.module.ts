c0de7178a126d678ea37439354c2983034b6e3d6
Roberto-Ceschini <robert17ceschini@gmail.com>
2024-09-16 19:34:00 -0300

    aff

--- ddd-e-commerce/src/registro_produto_filial/registro_produto_filial.module.ts
+++ ddd-e-commerce/src/registro_produto_filial/registro_produto_filial.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegistroProdutoFilialService } from './registro_produto_filial.service';
 import { RegistroProdutoFilialController } from './registro_produto_filial.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RegistroProdutoFilialController],
-  providers: [RegistroProdutoFilialService],
+  providers: [RegistroProdutoFilialService, PrismaService],
 })
 export class RegistroProdutoFilialModule {}
