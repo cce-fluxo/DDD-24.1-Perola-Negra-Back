b1ddfd7a0ab91a00eee5bba0a789a6f2595eaef9
Roberto-Ceschini <134228152+Roberto-Ceschini@users.noreply.github.com>
2024-11-02 16:55:57 -0300

    Update main.ts

--- ddd-e-commerce/src/main.ts
+++ ddd-e-commerce/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['localhost:3000', 'http://localhost:3000'],
+    origin: 'http://localhost:3000/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   const config = new DocumentBuilder()
